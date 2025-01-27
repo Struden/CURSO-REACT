89eb0f6542a7df79e8d811254111c17c8237ed2f
Diego <santid.net@gmail.com>
2025-01-27 10:36:41 -0300

    subida

--- tp-modulo-2-santi-diego/src/pages/ProductDetails/ProductDetails.jsx
+++ tp-modulo-2-santi-diego/src/pages/ProductDetails/ProductDetails.jsx
@@ -1,4 +1,4 @@
-import { Layout } from "../../components/layout/Layout";
+import { Layout } from "../../components/Layout/Layout";
 import { useLocation } from 'react-router-dom';
 
 const ProductDetails = () => {
